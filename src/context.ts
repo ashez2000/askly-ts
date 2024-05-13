ffd7aa7cf2e8b92c7075ccfac6e6666476a6fa12
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2024-05-13 23:09:04 +0530

    Setup graphql codegen

---
+++ src/context.ts
@@ -0,0 +1,5 @@
+import { StandaloneServerContextFunctionArgument } from '@apollo/server/standalone'
+
+export type Context = {
+  req: StandaloneServerContextFunctionArgument
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,18 @@
 import fs from 'node:fs'
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
+import { Resolvers } from './__generated__/resolvers-types.js'
+import { Context } from './context.js'
 
 const typeDefs = fs.readFileSync('./schema.gql', 'utf-8')
 
-const resolvers = {
+const resolvers: Resolvers = {
   Query: {
     hello: () => 'Hello, world!',
   },
 }
 
-const server = new ApolloServer({
+const server = new ApolloServer<Context>({
   typeDefs,
   resolvers,
 })
